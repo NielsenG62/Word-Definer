cd94b68edbc00fc89074f7a11333f3b3ed43aab9
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-13 09:19:41 -0700

    add to capybara test to test update and delete functionality

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,12 +3,19 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exeptions, false)
 
-describe('create an word path', {:type => :feature}) do
+describe('create a word, edit it, then delete it', {:type => :feature}) do
   it('creates an word and then goes to the word page') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'Transformer')
     click_on('Add')
-    expect(page).to have_content('Transformer')
+    click_on('Transformer')
+    click_on('Edit word')
+    fill_in('name', :with => 'Robot')
+    click_on('Update')
+    click_on('Robot')
+    click_on('Edit word')
+    click_on('Delete word')
+    expect(page).to have_content('There are no words here!')
   end
 end
