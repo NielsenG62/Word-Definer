4892d6459b0dfad2a71a317dbfd932537372c714
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-13 08:22:54 -0700

    add failing second test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,4 +8,14 @@ describe('#Word') do
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it("saves a word") do
+      word1 = Word.new('Transformer', nil)
+      word1.save()
+      word2 = Word.new('Robot', nil)
+      word2.save()
+      expect(Word.all).to(eq([word1, word2]))
+    end
+  end
 end
