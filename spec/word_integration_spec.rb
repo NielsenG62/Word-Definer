eb19bd04dc79d5cc046324d9e46802748b5870fd
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-13 13:43:45 -0700

    add capybara testing for definitions

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,8 +3,13 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exeptions, false)
 
-describe('create a word, edit it, then delete it', {:type => :feature}) do
-  it('creates an word and then goes to the word page') do
+before(:each) do
+  Album.clear()
+  Song.clear()
+end
+
+describe('create a word path', {:type => :feature}) do
+  it('create a word, edit it, then delete it') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'Transformers')
@@ -17,5 +22,16 @@ describe('create a word, edit it, then delete it', {:type => :feature}) do
     click_on('Edit word')
     click_on('Delete word')
     expect(page).to have_no_content('Robots')
+  end
+end
+
+describe('create a song path', {:type => :feature}) do
+  it('Creates a word. Creates, updates, and deletes a definition') do
+    test_word = Word.new('Rock', 100)
+    test_word.save
+    visit("/words/#{test_word.id}")
+    fill_in('definition_name', :with => 'A piece of earth')
+    click_on('Add definition')
+    expect(page).to have_content('A piece of earth')
   end
 end
