a93b787134fd0e41234a6599075f45c0a9ec5bec
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-13 09:43:49 -0700

    add test to definition to add to equality operator

---
+++ spec/definition_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'word'
+require 'definition'
+require 'pry'
+
+describe('#definition') do
+
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another definition") do
+      definition1 = Definition.new("Robot in disguise", @word.id, nil)
+      definition2 = Definition.new("Robot in disguise", @word.id, nil)
+      expect(definition1).to(eq(definition2))
+    end
+  end
+end
