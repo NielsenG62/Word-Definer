e12c1298daebf8940cabb03d2f86398ca781fffd
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-22 10:43:38 -0700

    update capybara testing to have a test for every CRUD function

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,42 +1,76 @@
 require('capybara/rspec')
 require('./app')
+require('pry')
 Capybara.app = Sinatra::Application
 set(:show_exeptions, false)
 
-before(:each) do
-  Album.clear()
-  Song.clear()
-end
+describe('The word Path', {:type => :feature}) do
 
-describe('create a word path', {:type => :feature}) do
-  it('create a word, edit it, then delete it') do
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @test_word = Word.new('Rock', nil)
+    @test_word.save
+    @test_word_id = @test_word.id
+  end
+
+  it('Creates a word') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'Transformers')
     click_on('Add')
-    click_on('Transformers')
+    expect(page).to have_content('Transformers')
+  end
+
+  it('updates a word') do
+    visit('/')
+    click_on('Rock')
     click_on('Edit word')
     fill_in('name', :with => 'Robots')
     click_on('Update')
-    click_on('Robots')
-    click_on('Edit word')
-    click_on('Delete word')
-    expect(page).to have_no_content('Robots')
+    expect(page).to have_content('Robots')
+  end
+
+  it('deletes a word') do
+    visit("/words/#{@test_word_id}/edit")
+    click_button('Delete word')
+    expect(page).to have_no_content('Rock')
   end
 end
 
-describe('create a song path', {:type => :feature}) do
-  it('Creates a word. Creates, updates, and deletes a definition') do
-    test_word = Word.new('Rock', 100)
-    test_word.save
-    visit("/words/#{test_word.id}")
+describe('The definition path', {:type => :feature}) do
+
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @test_word = Word.new('Rock', nil)
+    @test_word.save
+    @test_word_id = @test_word.id
+  end
+
+  it('Creates a definition') do
+    visit("/words/#{@test_word_id}")
     fill_in('definition_name', :with => 'A piece of earth')
     click_on('Add definition')
-    click_on('A piece of earth')
-    fill_in('name', :with => 'Not just a boulder')
+    expect(page).to have_content('A piece of earth')
+  end
+
+  it('Updates a definition') do
+    test_definition = Definition.new('Not just a boulder', @test_word_id, nil)
+    test_definition.save
+    test_definition_id = test_definition.id.to_i
+    visit("/words/#{@test_word_id}/definitions/#{test_definition_id}")
+    fill_in('name', :with => "It's a rock")
     click_on('Update')
-    click_on('Not just a boulder')
+    expect(page).to have_content("It's a rock")
+  end
+
+  it('Deletes a definition') do
+    test_definition = Definition.new('Not just a boulder', @test_word_id, nil)
+    test_definition.save
+    test_definition_id = test_definition.id.to_i
+    visit("/words/#{@test_word_id}/definitions/#{test_definition_id}")
     click_on('Delete definition')
-    expect(page).to have_no_content('A piece of earth')
+    expect(page).to have_no_content("Not just a boulder")
   end
 end
